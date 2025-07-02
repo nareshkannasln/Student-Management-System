20877d717ede4d49de235d833f18147fdc5b0024
nareshkannasln <nareshkannashanmugam@gmail.com>
2025-07-02 16:51:39 +0530

    newly developed

--- sm/student_management/doctype/class/class.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Nareshkanna and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Class", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- sm/student_management/doctype/subjects/subjects.py
+++ sm/student_management/doctype/subjects/subjects.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class Subjects(Document):
+class subjectss(Document):
 	pass
